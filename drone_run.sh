738b2c1d243a6ffb6278de45aaac25dccf0413e6
Sandeep Chinchali <vss.emvr@gmail.com>
2018-07-02 19:06:15 -0700

    update drone run to not need hardcoded paths

--- drone_run.sh
+++ drone_run.sh
@@ -1,8 +1,8 @@
 # change this to be based on your directory
-CONFIG_PATH=$RL_RESULT_DIR/docs/drone_config.ini
+CONFIG_PATH=$RL_SRC_DIR/docs/drone_config.ini
 
 # same as in config.ini under TRAIN_CONFIG
-BASE_DIR=$BASE_RL_RESULTS_DIR/drone_with_RL_results/
+BASE_DIR=$RL_RESULT_DIR/drone_with_RL_results/
 # clean the results
 rm -rf $BASE_DIR
 mkdir -p $BASE_DIR
